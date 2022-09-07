b70b04eb8eb8fc3cb7689f3e4caad69df7847f50
Dongwook Kim <ddong3852@gmail.com>
2022-09-07 14:39:49 +0900

    Update

--- CPU/CPU_top.v
+++ CPU/CPU_top.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module CPU #(
-    parameter DWIDTH 16,
-    parameter ADDR_WIDTH 12
+    parameter DWIDTH     = 16,
+    parameter ADDR_WIDTH = 12
 )   (
     input clk,
     input reset_n
