79d9fb72832d746f63af543cbcd981310ef8e3eb
Dongwook Kim <ddong3852@gmail.com>
2022-11-12 22:54:13 +0900

    update

--- SoC_design/HW_8/SSDC.h
+++ SoC_design/HW_8/SSDC.h
@@ -7,10 +7,10 @@ void process_sramq();
 void process_dramq();
 void process_nandq();
 
-#define SRAM_W_d = 4096;
-#define SRAM_R_d = 4096;
-#define DRAM_W_d = 4416;
-#define DRAM_R_d = 4800;
-#define NAND_W_d = 1402750;
+#define t_SRAM_W = 4096;
+#define t_SRAM_R = 4096;
+#define t_DRAM_W = 4416;
+#define t_DRAM_R = 4800;
+#define t_NAND_W = 1402750;
 
 #endif
--- SoC_design/HW_8/hw#8.c
+++ SoC_design/HW_8/hw#8.c
@@ -13,6 +13,12 @@ struct file
 int main(void)
 {
     int global_time = 0;
+    int time        = 0;
+ 
+    // busy
+    int sram_busy;
+    int dram_busy;
+    int nand_busy;
     
     // files
     struct file file[3];
@@ -23,17 +29,25 @@ int main(void)
     int nand[3] = {};
     FILE * fp;
     
+    // storing requests from host
     fp = fopen("host_request.txt", "rt");
     for(int i = 0; i < 3; i++) {
         fscanf(fp, "%d %d %d %d", &file[i].name, &file[i].w_r, &file[i].size, &file[i].t_arrival);
         printf("%d, %d, %d, %d\n", file[i].name, file[i].w_r, file[i].size, file[i].t_arrival);
+
+        time        = file[i].t_arrival;
+        global_time = time;
     }
     fclose(fp);
 
     //while (1)
     //{
-        
+    //    
+//
+    //    time++;
     //}
     
+    printf("result global time is %d ns\n", global_time);
+
     return 0;
 }
