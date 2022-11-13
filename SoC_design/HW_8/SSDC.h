084c0eb1c6a5a97bb6bf8f76611f3530a7553419
Dongwook Kim <ddong3852@gmail.com>
2022-11-13 16:11:58 +0900

    update

--- SoC_design/HW_8/SSDC.h
+++ SoC_design/HW_8/SSDC.h
@@ -1,16 +1,12 @@
 #ifndef _SSDC.H_
 #define _SSDC.H_
+
 #include <stdio.h>
 
-void process_pcieq();
-void process_sramq();
-void process_dramq();
-void process_nandq();
-
-#define t_SRAM_W = 4096;
-#define t_SRAM_R = 4096;
-#define t_DRAM_W = 4416;
-#define t_DRAM_R = 4800;
-#define t_NAND_W = 1402750;
+#define t_SRAM_W  4096
+#define t_SRAM_R  4096
+#define t_DRAM_W  4416
+#define t_DRAM_R  4800
+#define t_NAND_W  1402750 // = 1402.75 μs
 
 #endif
--- SoC_design/HW_8/hw#8.c
+++ SoC_design/HW_8/hw#8.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include <SSDC.h>
+#include "SSDC.h"
 
 struct file
     {
@@ -8,26 +8,27 @@ struct file
         int size;
         int t_arrival;
     };
-    
 
 int main(void)
 {
     int global_time = 0;
     int time        = 0;
- 
-    // busy
-    int sram_w_busy = 0;
-    int dram_w_busy = 0;
-    int nand_w_busy = 0;
     
     // files
     struct file file[3];
     FILE * fp;
 
+    // busy & empty
+    int pcieq_empty;
+    int sram_w_busy;
+    int sram_r_busy;
+    int dram_w_busy;
+    int nand_w_busy;
+
     // array of each device
-    int sram[3] = {};
-    int dram[3] = {};
-    int nand[3] = {};
+    int sram[3] = {0, 0, 0};
+    int dram[3] = {0, 0, 0};
+    int nand[3] = {0, 0, 0};
     
     
     // storing requests from host
@@ -36,20 +37,64 @@ int main(void)
         fscanf(fp, "%d %d %d %d", &file[i].name, &file[i].w_r, &file[i].size, &file[i].t_arrival);
         printf("%d, %d, %d, %d\n", file[i].name, file[i].w_r, file[i].size, file[i].t_arrival);
 
+        pcieq_empty = 0;
         sram_w_busy = 1;
-
         time        = file[i].t_arrival;
         global_time = time;
+
+        if(i == 0){
+            sram[i] = sram[i] + t_SRAM_W;
+        }
+        else{
+            sram[i] = sram[i-1] + t_SRAM_W;
+        }
     }
     fclose(fp);
+    printf("time is %d\n", time);
+    printf("global time is %d\n", global_time);
+    printf("%d, %d, %d\n", sram[0], sram[1], sram[2]);
 
-    while (sram_w_busy || dram_w_busy || nand_w_busy)
+    while (pcieq_empty || sram_w_busy || sram_r_busy || dram_w_busy || nand_w_busy)
     {
+        process_pcieq(time, pcieq_empty, sram_w_busy);
+        process_sramq();
+        process_dramq();
+        process_nandq();
+
         
+
         time++;
     }
     
     printf("result global time is %d ns\n", global_time);
 
     return 0;
+}
+
+// function
+void process_pcieq(int time, int empty, int busy) 
+{
+    if(!empty && !busy){
+        if(time){
+
+        }
+    }
+    else if(empty){
+        busy = 0;
+    }
+}
+
+void process_sramq()
+{
+    
+}
+
+void process_dramq()
+{
+    
+}
+
+void process_nandq()
+{
+    
 }
