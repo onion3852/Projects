5dacf19a4e13ae10f7e2fd959819bcfd19117eb5
Dongwook Kim <ddong3852@gmail.com>
2022-11-13 13:37:12 +0900

    update

--- SoC_design/HW_8/hw#8.c
+++ SoC_design/HW_8/hw#8.c
@@ -16,18 +16,19 @@ int main(void)
     int time        = 0;
  
     // busy
-    int sram_busy;
-    int dram_busy;
-    int nand_busy;
+    int sram_w_busy = 0;
+    int dram_w_busy = 0;
+    int nand_w_busy = 0;
     
     // files
     struct file file[3];
+    FILE * fp;
 
     // array of each device
     int sram[3] = {};
     int dram[3] = {};
     int nand[3] = {};
-    FILE * fp;
+    
     
     // storing requests from host
     fp = fopen("host_request.txt", "rt");
@@ -35,17 +36,18 @@ int main(void)
         fscanf(fp, "%d %d %d %d", &file[i].name, &file[i].w_r, &file[i].size, &file[i].t_arrival);
         printf("%d, %d, %d, %d\n", file[i].name, file[i].w_r, file[i].size, file[i].t_arrival);
 
+        sram_w_busy = 1;
+
         time        = file[i].t_arrival;
         global_time = time;
     }
     fclose(fp);
 
-    //while (1)
-    //{
-    //    
-//
-    //    time++;
-    //}
+    while (sram_w_busy || dram_w_busy || nand_w_busy)
+    {
+        
+        time++;
+    }
     
     printf("result global time is %d ns\n", global_time);
 
