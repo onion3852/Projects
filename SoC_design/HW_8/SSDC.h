ed3b0783ec192a9c60ccf05d542dd660cfa00bc4
Dongwook Kim <ddong3852@gmail.com>
2022-11-14 16:47:31 +0900

    update

--- SoC_design/HW_8/SSDC.h
+++ SoC_design/HW_8/SSDC.h
@@ -1,12 +1,11 @@
 #ifndef _SSDC.H_
-#define _SSDC.H_
 
-#include <stdio.h>
+#define _SSDC.H_
 
 #define t_SRAM_W  4096
 #define t_SRAM_R  4096
 #define t_DRAM_W  4416
 #define t_DRAM_R  4800
-#define t_NAND_W  1402750 // = 1402.75 μs
+#define t_NAND_W  1402750
 
 #endif
