2c9e41dcc8665e06e483148aa60359edf0b0c51f
Dongwook Kim <ddong3852@gmail.com>
2022-11-14 22:52:44 +0900

    update

--- SoC_design/HW_8/hw#8.c
+++ SoC_design/HW_8/hw#8.c
@@ -9,9 +9,14 @@ struct host_request
         int t_arrival;
     };
 
-void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *w_busy, int *sram_num, int *sram, int *file_num, struct host_request *q);
-void process_sramq(int time, int *global_time, int *w_busy, int *s_empty, int *sram_num, int *sram, int *file_num, struct host_request *q);
+void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *s_w_busy,
+                   int *sram_num, int *sram, int *file_num, struct host_request *q);
 
+void process_sramq(int time, int *global_time, int *s_empty, int *d_empty, int *s_w_busy, int *d_w_busy,
+                   int *sram_num, int *sram, int *dram_num, int *dram);
+
+                   
+// main
 int main(void)
 {
     // time
@@ -62,43 +67,47 @@ int main(void)
     fclose(fp);
 
     // 
-    while (/*!pcieq_empty || !sramq_empty || !dramq_empty || !nandq_empty || sram_w_busy || dram_w_busy || nand_w_busy*/time < 8200)
+    while (/*!pcieq_empty || !sramq_empty || !dramq_empty || !nandq_empty || sram_w_busy || dram_w_busy || nand_w_busy*/time < 8195)
     {
         printf("while loop start !\n");
-        printf("current file_num is %d\n", file_num);
         printf("current time is %d\n", time);
         printf("--------------------\n\n");
         printf("process_check function start !\n");
-        process_pcieq(time, &global_time, &pcieq_empty, &sramq_empty, &sram_w_busy, &sram_num, sram, &file_num, &file[file_num]);
-        process_sramq(time, &global_time, &sram_w_busy, &sramq_empty, &sram_num, sram, &file_num, &file[file_num]);
+        process_pcieq(time, &global_time, &pcieq_empty, &sramq_empty, &sram_w_busy, 
+                      &sram_num, sram, &file_num, &file[file_num]);
+        process_sramq(time, &global_time, &sramq_empty, &dramq_empty, &sram_w_busy, &dram_w_busy, 
+                      &sram_num, sram, &dram_num, dram);
+                      
       //process_dramq();
       //process_nandq();
         time++;
     }
-
+    printf("global time : %d\n", global_time);
+    printf("");
     return 0;
 }
 
 
 // function part
-void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *w_busy, int *sram_num, int *sram, int *file_num, struct host_request *q) 
+void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *s_w_busy, 
+                   int *sram_num, int *sram, int *file_num, struct host_request *q) 
 {
-    if(*w_busy && (time < sram[*sram_num])){
-        // writting is in progress
-        printf("SRAM writting is in progress!!!\n");
-        printf("writting end time is %d / current time is %d\n", sram[*sram_num], time);
+    if(*s_w_busy && (time < sram[*sram_num])){
+        // sram writting is in progress
+        printf("SRAM writting for file[%d] is in progress!!!\n", *sram_num);
+        printf("SRAM writting for file[%d] end time is %d / current time is %d\n", *sram_num, sram[*sram_num], time);
     }
-    else if(!*w_busy && !*p_empty){
+    else if(!*s_w_busy && !*p_empty){
         // sram write task is done.
         // if sramq is empty(sram write port isn't busy) while pcieq isn't,
         // sramq will get new task
-        printf("sram write of next file will start !\n");
-        sram[*sram_num] = time + t_SRAM_W;
+        printf("sram write of file[%d] will start !\n", *sram_num);
+        sram[*sram_num] = time + t_SRAM_W - 1;
         printf("end time will be %d ns\n", sram[*sram_num]);
-        *w_busy  = 1;
+        *s_w_busy  = 1;
         *s_empty = 0;
 
-        // clear completed task in pcieq
+        // delete the completed task in pcieq
         if(*file_num <= 2){
             q -> name      = 0;
             q -> w_r       = 0;
@@ -107,7 +116,7 @@ void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *
             (*file_num)++;  // move to next file
         }
     }
-    else if(!*w_busy){
+    else if(!*s_w_busy){
         if((*file_num > 2)){
             // pcieq is empty now because all sram write task is done
             *p_empty = 1;
@@ -116,13 +125,14 @@ void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *
     return;
 }
 
-void process_sramq(int time, int *global_time, int *w_busy, int *s_empty, int *sram_num, int *sram, int *file_num, struct host_request *q)
+void process_sramq(int time, int *global_time, int *s_empty, int *d_empty, int *s_w_busy, int *d_w_busy,
+                   int *sram_num, int *sram, int *dram_num, int *dram)
 {
-    if(*w_busy && (time == sram[*sram_num])){
-        // sram write done, write port isn't busy now
-        // sramq is empty now
-        *w_busy  = 0;
-        *s_empty = 1;
+    if(*s_w_busy && (time == sram[*sram_num])){
+        // sram write done, sramq is emptyand write port isn't busy now
+        printf("SRAM write for file[%d] is done !\n", *sram_num);
+        *s_w_busy = 0;
+        *s_empty  = 1;
 
         // global time is changed to current time
         *global_time = time;
@@ -130,21 +140,23 @@ void process_sramq(int time, int *global_time, int *w_busy, int *s_empty, int *s
         // clear 'sram write end time' array of completed task
         sram[*sram_num] = 0;
         (*sram_num)++;
-
-        //
-        
-        return;
     }
-    if(!*w_busy){
-        if((*file_num <= 2)){
-            // start sram write for next file 
-            (*sram_num) ++; 
-            sram[*sram_num];
-            
-            *w_busy = 1;
+    else if(!*d_w_busy && !*d_empty){
+        // dram write task is done.
+        // if dramq is empty(dram write port isn't busy) while other task is left,
+        // dramq will get new task
+        printf("dram write of file[%d] will start !\n", *dram_num);
+        dram[*dram_num] = time + t_DRAM_W - 1;
+        printf("end time will be %d ns\n", dram[*dram_num]);
+        *d_w_busy = 1;
+        *d_empty  = 0;
         }
-        return;
+    if(*d_w_busy && (time < dram[*dram_num])){
+        // dram writting is in progress
+        printf("DRAM writting for file[%d] is in progress!!!\n", *dram_num);
+        printf("DRAM writting for file[%d] end time is %d / current time is %d\n", *dram_num, dram[*dram_num], time);
     }
+    return;
 }
 
 //void process_dramq()
