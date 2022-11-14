3b06185632bfe02eb19cb9b0cebdfbfa8030fd43
Dongwook Kim <ddong3852@gmail.com>
2022-11-14 21:59:11 +0900

    update

--- SoC_design/HW_8/hw#8.c
+++ SoC_design/HW_8/hw#8.c
@@ -10,7 +10,7 @@ struct host_request
     };
 
 void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *w_busy, int *sram_num, int *sram, int *file_num, struct host_request *q);
-// void process_sramq();
+void process_sramq(int time, int *global_time, int *w_busy, int *s_empty, int *sram_num, int *sram, int *file_num, struct host_request *q);
 
 int main(void)
 {
@@ -22,10 +22,6 @@ int main(void)
     int sram_num = 0;
     int dram_num = 0;
     int nand_num = 0;
-    int temp_p = 0;  // pcieq temp variable
-    int temp_s = 0;  // sramq temp variable
-    int temp_d = 0;  // dramq temp variable
-    int temp_n = 0;  // nandq temp variable
 
     // files
     struct host_request file[3];
@@ -41,45 +37,40 @@ int main(void)
     int dram_w_busy = 0;
     int nand_w_busy = 0;
 
-    // array of each device
+    // array of writting end time for each device
     int sram[3] = {0, 0, 0};
     int dram[3] = {0, 0, 0};
     int nand[3] = {0, 0, 0};
     
     
-    // storing requests from host
+    // storing requests from host using fscanf()
     fp = fopen("host_request.txt", "rt");
     for(int i = 0; i < 3; i++) {
         fscanf(fp, "%d %d %d %d", &file[i].name, &file[i].w_r, &file[i].size, &file[i].t_arrival);
-        printf("%d, %d, %d, %d\n", file[i].name, file[i].w_r, file[i].size, file[i].t_arrival);
+        printf("%d, %d, %d, %d\n", file[i].name,  file[i].w_r,  file[i].size,  file[i].t_arrival);
 
-        pcieq_empty = 0;
-        sram_w_busy = 1;
+        pcieq_empty = 0;    // pcieq is now not empty
+        sram_w_busy = 1;    // SRAM write for the 1st file begins
         if(i == 0){
-            sram[i] = sram[i] + t_SRAM_W;
-            temp_s  = sram[0];
+            sram[i] = sram[i] + t_SRAM_W;  // SRAM wirte end time(= 4096 ns) for the 1st file
         }
-
+        
+        // set global time when host_request arrives
         time        = file[i].t_arrival;
         global_time = time;
     }
     fclose(fp);
-    printf("time is %d\n", time);
-    printf("global time is %d\n", global_time);
-    printf("%d, %d, %d\n", sram[0], sram[1], sram[2]);
-    printf("sram busy is %d\n", sram_w_busy);
-    printf("sram_num is %d\n", sram_num);
-    printf("sram[sram_num] is %d\n", sram[sram_num]);
 
+    // 
     while (/*!pcieq_empty || !sramq_empty || !dramq_empty || !nandq_empty || sram_w_busy || dram_w_busy || nand_w_busy*/time < 8200)
     {
-        printf("--------------------\n");
-        printf("file_num is %d\n", file_num);
-        printf("sram_num is %d\n", sram_num);
-        printf("time is %d\n", time);
-        printf("--------------------\n\n\n");
+        printf("while loop start !\n");
+        printf("current file_num is %d\n", file_num);
+        printf("current time is %d\n", time);
+        printf("--------------------\n\n");
+        printf("process_check function start !\n");
         process_pcieq(time, &global_time, &pcieq_empty, &sramq_empty, &sram_w_busy, &sram_num, sram, &file_num, &file[file_num]);
-      //process_sramq(time, global_time, sram_w_busy, sram_num, sram[sram_num], file_num, &file[file_num]);
+        process_sramq(time, &global_time, &sram_w_busy, &sramq_empty, &sram_num, sram, &file_num, &file[file_num]);
       //process_dramq();
       //process_nandq();
         time++;
@@ -92,42 +83,29 @@ int main(void)
 // function part
 void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *w_busy, int *sram_num, int *sram, int *file_num, struct host_request *q) 
 {
-    if(*w_busy && (time >= sram[*sram_num])){
-        // sram write done...
-        // clear pcieq of completed request
-        printf("first condition!!!\n");
+    if(*w_busy && (time < sram[*sram_num])){
+        // writting is in progress
+        printf("SRAM writting is in progress!!!\n");
+        printf("writting end time is %d / current time is %d\n", sram[*sram_num], time);
+    }
+    else if(!*w_busy && !*p_empty){
+        // sram write task is done.
+        // if sramq is empty(sram write port isn't busy) while pcieq isn't,
+        // sramq will get new task
+        printf("sram write of next file will start !\n");
+        sram[*sram_num] = time + t_SRAM_W;
+        printf("end time will be %d ns\n", sram[*sram_num]);
+        *w_busy  = 1;
+        *s_empty = 0;
+
+        // clear completed task in pcieq
         if(*file_num <= 2){
             q -> name      = 0;
             q -> w_r       = 0;
             q -> size      = 0;
             q -> t_arrival = 0;
-            (*file_num)++;
-
-            *global_time = time;    // sram write done time is now the global time 
-            *w_busy      = 0;       // sram write port isn't busy
-            *s_empty     = 1;       // sramq is empty
-
-            // clear sram array of completed request 
-            sram[*sram_num] = 0;
-
-            printf("sram_num is %d\n", *sram_num);
-            printf("file_num is %d\n", *file_num);
-            printf("global time is %d\n", *global_time);
-            printf("sram[sram_num]is %d\n", sram[*sram_num]);
-        
-            // start sram write for next file
-            // sramq becomes not empty
-            (*sram_num)++;
-            printf("start new file write to sram!!!\n");
-            printf("sram_num is %d\n", *sram_num);
-            sram[*sram_num] = time + t_SRAM_W;
-            *w_busy  = 1;
-            *s_empty = 0;
+            (*file_num)++;  // move to next file
         }
-    }
-    else if(*w_busy && (time < sram[*sram_num])){
-        printf("sram writting is in progress!!!\n");
-        printf("writting end time is %d\n", sram[*sram_num]);
     }
     else if(!*w_busy){
         if((*file_num > 2)){
@@ -138,30 +116,32 @@ void process_pcieq(int time, int *global_time, int *p_empty, int *s_empty, int *
     return;
 }
 
-void process_sramq(int time, int global_time, int temp_p, int w_busy, int sram_num, int sram[sram_num], int file_num, struct host_request *q)
+void process_sramq(int time, int *global_time, int *w_busy, int *s_empty, int *sram_num, int *sram, int *file_num, struct host_request *q)
 {
-    if(w_busy && (time >= temp_p)){
-        // sram write done...
-        // clear pcieq of completed request
-        if(file_num <= 2){
-            q -> name      = 0;
-            q -> w_r       = 0;
-            q -> size      = 0;
-            q -> t_arrival = 0;
-            file_num++;
+    if(*w_busy && (time == sram[*sram_num])){
+        // sram write done, write port isn't busy now
+        // sramq is empty now
+        *w_busy  = 0;
+        *s_empty = 1;
 
-            global_time = time;
-            w_busy      = 0;
-        }
+        // global time is changed to current time
+        *global_time = time;
+
+        // clear 'sram write end time' array of completed task
+        sram[*sram_num] = 0;
+        (*sram_num)++;
+
+        //
+        
         return;
     }
-    if(!w_busy){
-        if((file_num <= 2)){
+    if(!*w_busy){
+        if((*file_num <= 2)){
             // start sram write for next file 
-            sram_num ++; 
-            temp_p = sram[sram_num];
+            (*sram_num) ++; 
+            sram[*sram_num];
             
-            w_busy = 1;
+            *w_busy = 1;
         }
         return;
     }
